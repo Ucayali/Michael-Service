d3871f00b05c4712899ffb910dfc34c3f25bf96e
Michael <griffinvmichael@gmail.com>
2020-06-01 15:48:37 -0700

    artillery tweaks forgot to push

--- server/database/old_config.js
+++ server/database/old_config.js
@@ -1,65 +1,62 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-console */
-/* eslint-disable func-names */
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost:27017/singleItemPage?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
-mongoose.connect(uri, { useNewUrlParser: true });
-const { Schema } = mongoose;
+// const uri = 'mongodb://localhost:27017/singleItemPage?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false';
+// mongoose.connect(uri, { useNewUrlParser: true });
+// const { Schema } = mongoose;
 
 
-const itemSchema = new Schema({
-  itemId: Number,
-  altImages: [String],
-});
+// const itemSchema = new Schema({
+//   itemId: Number,
+//   altImages: [String],
+// });
 
-const Item = mongoose.model('Items', itemSchema);
+// const Item = mongoose.model('Items', itemSchema);
 
-const createItem = function (obj, cb = () => {}) {
-  const item = new Item(obj);
+// const createItem = function (obj, cb = () => {}) {
+//   const item = new Item(obj);
 
-  item.save((err) => {
-    if (err) { cb(err, null); } else { cb(null, 'congrats'); }
-  });
-};
+//   item.save((err) => {
+//     if (err) { cb(err, null); } else { cb(null, 'congrats'); }
+//   });
+// };
 
-const deleteAll = function () {
-  Item.deleteMany({}, (err) => {
-    if (err) { console.log(err); }
-  });
-};
+// const deleteAll = function () {
+//   Item.deleteMany({}, (err) => {
+//     if (err) { console.log(err); }
+//   });
+// };
 
-const deleteById = (id, cb) => {
-  Item.deleteOne({ itemId: id }, (err, item) => {
-    if (err) {
-      return cb(err, null);
-    }
-    return cb(null, item);
-  });
-};
+// const deleteById = (id, cb) => {
+//   Item.deleteOne({ itemId: id }, (err, item) => {
+//     if (err) {
+//       return cb(err, null);
+//     }
+//     return cb(null, item);
+//   });
+// };
 
-const updateById = (id, cb) => {
-  Item.updateOne({ itemId: id }, (err, item) => {
-    if (err) {
-      return cb(err, null);
-    }
-    return cb(null, item);
-  });
-};
+// const updateById = (id, cb) => {
+//   Item.updateOne({ itemId: id }, (err, item) => {
+//     if (err) {
+//       return cb(err, null);
+//     }
+//     return cb(null, item);
+//   });
+// };
 
-const getItem = (id, cb) => {
-  Item.findOne({ itemId: id }, (err, item) => {
-    if (err) {
-      return cb(err, null);
-    }
-    return cb(null, item);
-  });
-};
+// const getItem = (id, cb) => {
+//   Item.findOne({ itemId: id }, (err, item) => {
+//     if (err) {
+//       return cb(err, null);
+//     }
+//     return cb(null, item);
+//   });
+// };
 
-module.exports = {
-  createItem,
-  deleteAll,
-  deleteById,
-  getItem,
-  updateById,
-};
+// module.exports = {
+//   createItem,
+//   deleteAll,
+//   deleteById,
+//   getItem,
+//   updateById,
+// };
